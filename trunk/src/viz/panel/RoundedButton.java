9757d44818df0f2733073addc9390ce300673327
higgs.ml@gmail.com <higgs.ml@gmail.com@f276de63-0bba-6af2-2dc0-9b7bbd3ce67b>
2012-06-13 08:30:25 +0000

    added rounded button

---
+++ trunk/src/viz/panel/RoundedButton.java
@@ -0,0 +1,67 @@
+package viz.panel;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.RoundRectangle2D;
+
+import javax.swing.JButton;
+
+
+public class RoundedButton extends JButton implements MouseListener {
+
+	boolean mouseIn = false;
+
+	public RoundedButton(String label) {
+		super(label);
+        setBorderPainted(false);
+        addMouseListener(this);
+	}
+
+	@Override
+    protected void paintComponent(Graphics g)
+    {
+		Graphics2D g2 = (Graphics2D) g;
+        if (getModel().isPressed()) {
+            g.setColor(g.getColor());
+            g2.fillRect(3, 3, getWidth() - 6, getHeight() - 6);
+        }
+        super.paintComponent(g);
+
+        if (mouseIn)
+            g2.setColor(Color.darkGray);
+        else
+            g2.setColor(new Color(128, 128, 128));
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setStroke(new BasicStroke(1.2f));
+        g2.draw(new RoundRectangle2D.Double(1, 1, (getWidth() - 3),
+                (getHeight() - 3), 12, 8));
+        g2.setStroke(new BasicStroke(1.5f));
+        g2.drawLine(4, getHeight() - 3, getWidth() - 4, getHeight() - 3);
+
+        g2.dispose();
+    }
+
+	public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        mouseIn = true;
+    }
+
+    public void mouseExited(MouseEvent e) {
+        mouseIn = false;
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+}
