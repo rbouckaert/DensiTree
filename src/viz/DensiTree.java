b1c13e3a9145176d40981fffd512034f82c04684
higgs.ml <higgs.ml@f276de63-0bba-6af2-2dc0-9b7bbd3ce67b>
2012-08-13 02:58:22 +0000

    make mac friendly

--- src/viz/DensiTree.java
+++ src/viz/DensiTree.java
@@ -50,6 +50,7 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.awt.print.*;
 import java.io.*;
+import java.lang.reflect.Method;
 import java.text.DecimalFormat;
 import java.util.*;
 import java.util.List;
@@ -4926,18 +4927,68 @@ public class DensiTree extends JPanel implements ComponentListener {
 		helpMenu.add(a_viewClades);
 		helpMenu.add(a_about);
 	} // makeMenuBar
+	
+    public static void loadUIManager() {
+        boolean lafLoaded = false;
+
+        if (isMac()) {
+            System.setProperty("apple.awt.graphics.UseQuartz", "true");
+            System.setProperty("apple.awt.antialiasing", "true");
+            System.setProperty("apple.awt.rendering", "VALUE_RENDER_QUALITY");
+
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            System.setProperty("apple.awt.draggableWindowBackground", "true");
+            System.setProperty("apple.awt.showGrowBox", "true");
+
+            try {
+
+                try {
+                    // We need to do this using dynamic class loading to avoid other platforms
+                    // having to link to this class. If the Quaqua library is not on the classpath
+                    // it simply won't be used.
+                    Class<?> qm = Class.forName("ch.randelshofer.quaqua.QuaquaManager");
+                    Method method = qm.getMethod("setExcludedUIs", Set.class);
+
+                    Set<String> excludes = new HashSet<String>();
+                    excludes.add("Button");
+                    excludes.add("ToolBar");
+                    method.invoke(null, excludes);
+
+                } catch (Throwable e) {
+                }
+
+                //set the Quaqua Look and Feel in the UIManager
+                UIManager.setLookAndFeel(
+                        "ch.randelshofer.quaqua.QuaquaLookAndFeel"
+                );
+                lafLoaded = true;
+
+            } catch (Exception e) {
+
+            }
+
+            UIManager.put("SystemFont", new Font("Lucida Grande", Font.PLAIN, 13));
+            UIManager.put("SmallSystemFont", new Font("Lucida Grande", Font.PLAIN, 11));
+        }
+
+        try {
+
+            if (!lafLoaded) {
+                UIManager.setLookAndFeel("javax.swing.plaf.metal");
+                //UIManager.getSystemLookAndFeelClassName());
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    public static boolean isMac() {
+        return System.getProperty("os.name").toLowerCase().startsWith("mac");
+    }
 
 	public static DensiTree startNew(String [] args) {
 		DensiTree a = new DensiTree(args);
 		
-		try {
-			String laff = UIManager.getSystemLookAndFeelClassName();
-			laff = "javax.swing.plaf.metal.MetalLookAndFeel";
-			System.setProperty("swing.metalTheme", "steel");
-			UIManager.setLookAndFeel(laff);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+		loadUIManager();
 		
 		JFrame f;
 		f = new JFrame(FRAME_TITLE);
